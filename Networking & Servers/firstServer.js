0dfa62ef12c5a7eb141da3dd3c53fe41220b902f
Azharuddin Ali <azharuddinali017@gmail.com>
2025-10-23 21:45:16 +0530

    palyed with request and response in plain node

--- Networking & Servers/firstServer.js
+++ Networking & Servers/firstServer.js
@@ -1,7 +1,14 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  // console.log(req);
+  console.log(req.url ,req.method, req.headers, );
+  res.setHeader('Content-Type','text/html');
+  res.write('<html>');
+  res.write('<head><title>Hello world</title></head>');
+  res.write('<body><h1>hello Azhar</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 port = 3001;
